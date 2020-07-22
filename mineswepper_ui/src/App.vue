19334bb81c469b064ef22fa984aaf8e976e39d13
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2020-07-22 13:20:46 +0200

    Initialized VueJS app

---
+++ mineswepper_ui/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <PlayerPanel />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import PlayerPanel from "./components/PlayerPanel.vue";
+
+export default Vue.extend({
+  name: "App",
+  components: {
+    PlayerPanel
+  }
+});
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ mineswepper_ui/vue.config.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+module.exports = {
+  devServer: {
+    disableHostCheck: true,
+    https: {
+      key: fs.readFileSync(process.env.SSL_KEY),
+      cert: fs.readFileSync(process.env.SSL_CERT)
+    }
+  }
+};
